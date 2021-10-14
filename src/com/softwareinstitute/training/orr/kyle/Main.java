2e9a3cb50dc304f05f25759b6d6859aea04b5994
Kyle Orr <KORR202@caledonian.ac.uk>
2021-10-14 19:38:44 +0100

    Have added some extra lines in the output so it reads better

--- src/com/softwareinstitute/training/orr/kyle/Main.java
+++ src/com/softwareinstitute/training/orr/kyle/Main.java
@@ -8,12 +8,12 @@ public class Main {
 
         ArrayList<LibraryContents> LibraryContents = new ArrayList<>(10);
         LibraryContents.add(new Book("To Kill a Mockingbird" , "Classic","Harper Lee"));
-        System.out.println("Title: "+ Book.getTitle() +", written by " + Book.getAuthor()+", and this book is "+ Book.getGenre());
+        System.out.println("Title: "+ Book.getTitle() +", written by " + Book.getAuthor()+", and this book's genre is: "+ Book.getGenre()+ ".");
 
-        LibraryContents.add(new DVD("Batman: The Dark Knight", "It is a Fantasy movie", "Christopher Nolan", 152));
-        System.out.println("Title: "+ DVD.getTitle()+", "+ DVD.getGenre()+ ", directed by " + DVD.getDirector() +", and it plays for "+DVD.getDVDLength()+ " minutes");
+        LibraryContents.add(new DVD("Batman: The Dark Knight", "Fantasy", "Christopher Nolan", 152));
+        System.out.println("Title: "+ DVD.getTitle()+", this film's genre is: " + DVD.getGenre()+ ", directed by " + DVD.getDirector() +", and it plays for "+DVD.getDVDLength()+ " minutes.");
 
         LibraryContents.add(new Audiobook("Wolf of Wall Street", "Biopic", 1200));
-        System.out.println("Title: "+ Audiobook.getTitle()+ ", It is a "+ Audiobook.getGenre()+ ", which plays for " + Audiobook.getABLength()+ " minutes");
+        System.out.println("Title: "+ Audiobook.getTitle()+ ", this audiobook's genre is: "+ Audiobook.getGenre()+ ", which plays for " + Audiobook.getABLength()+ " minutes.");
     }
 }
