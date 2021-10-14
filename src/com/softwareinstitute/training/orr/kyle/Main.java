27ba31d3eb353c49d9cb57213f044ebc92cbeba0
Kyle Orr <KORR202@caledonian.ac.uk>
2021-10-14 22:05:04 +0100

    Changed the name of the Auiobook to the correct name, was missing 'The' at the start

--- src/com/softwareinstitute/training/orr/kyle/Main.java
+++ src/com/softwareinstitute/training/orr/kyle/Main.java
@@ -13,7 +13,7 @@ public class Main {
         LibraryContents.add(new DVD("Batman: The Dark Knight", "Fantasy", "Christopher Nolan", 152));
         System.out.println("Title (Film) : "+ DVD.getTitle()+", this film's genre is: " + DVD.getGenre()+ ", is directed by " + DVD.getDirector() +", and plays for "+DVD.getDVDLength()+ " minutes.");
 
-        LibraryContents.add(new Audiobook("Wolf of Wall Street", "Biopic", 1200));
+        LibraryContents.add(new Audiobook("The Wolf of Wall Street", "Biopic", 1200));
         System.out.println("Title (Audiobook) : "+ Audiobook.getTitle()+ ", this audiobook's genre is: "+ Audiobook.getGenre()+ ", which plays for " + Audiobook.getABLength()+ " minutes.");
 
         LibraryContents.add(new Novel("To Kill a Mockingbird", "Classic", "Harper Lee", 281));
