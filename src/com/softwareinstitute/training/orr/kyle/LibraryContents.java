4ee8df3bed9dd0faf8fbb5d44e0ab8b4578afbc9
Kyle Orr <KORR202@caledonian.ac.uk>
2021-10-14 21:48:12 +0100

    Final push of OOP Library code

--- src/com/softwareinstitute/training/orr/kyle/LibraryContents.java
+++ src/com/softwareinstitute/training/orr/kyle/LibraryContents.java
@@ -1,12 +1,10 @@
 package com.softwareinstitute.training.orr.kyle;
 
-
 public abstract class LibraryContents {
 
 
     private static String Title;
     private static String Genre;
-
 
 
     public LibraryContents(String title, String genre) {
@@ -15,21 +13,18 @@ public abstract class LibraryContents {
     }
 
 
-
     public static String getTitle() {
         return Title;
     }
     public void setTitle(String Title) {
         this.Title = Title;
     }
-
     public static String getGenre(){
         return Genre;
     }
     public void setGenre(){
         this.Genre = Genre;
     }
-
 }
 
 
