982d2ce3e72d92e6f25cedeb3a3d153d2e8a408a
Kyle Orr <KORR202@caledonian.ac.uk>
2021-10-15 10:42:31 +0100

    Have added a few more unit tests

--- src/com/softwareinstitute/training/orr/kyle/testLibrary.java
+++ src/com/softwareinstitute/training/orr/kyle/testLibrary.java
@@ -6,10 +6,28 @@ import static org.junit.Assert.assertEquals;
 
 public class testLibrary {
 
-@Test
+    @Test
     public void testBookTitleMethod(){
 
     Book testBook = new Book ("Harry Potter", "Fantasy", "JK Rowling",234);
-    assertEquals("The Book is Harry Potter", "The Book is Harry Potter");
+    assertEquals("The Book is not Harry Potter", "Harry Potter", "Harry Potter");
+    }
+    @Test
+    public void testBookGenreMethod(){
+
+        Book testBook = new Book ("Harry Potter", "Fantasy", "JK Rowling",234);
+        assertEquals("The Book is not Fantasy", "Fantasy", "Fantasy");
+    }
+    @Test
+    public void testDVDTitleMethod(){
+
+        DVD testDVD = new DVD ("IT", "Horror", "Stephen King",96);
+        assertEquals("The Film is not IT", "IT", "IT");
+    }
+    @Test
+    public void testDVDGenreMethod(){
+
+        DVD testDVD = new DVD ("IT", "Horror", "Stephen King",96);
+        assertEquals("The Film is not Horror", "Horror", "Horror");
     }
 }
